048388bb4979ebcae3105c8ccac86dae2ee362e8
Karla Nieves <karlaxnieves@gmail.com>
2021-07-08 15:52:05 -0700

    wip

--- index.js
+++ index.js
@@ -1,7 +1,29 @@
 require('dotenv')
 const express = require('express')
+const cors = require('cors')
+
 const server = express()
-PORT = process.env.PORT || 5000
+
+const PORT = process.env.PORT || 9000
+
+server.use(express.json())
+server.use(cors())
+
+server.get('/api/hello', (req, res) => {
+    res.json({ message: 'api is working'})
+})
+
+server.use('*', (req, res)=> {
+    res.send(`<h1>Hello, there</h1>`)
+})
+
+server.use((err, req, res, next) => { //eslint-disable-line
+    res.status(500).json({
+        message: err.message,
+        stack: err.stack,
+    })
+})
+
 server.listen(PORT, () => {
-    console.log(`listening to ${PORT}`)
+    console.log(`listening on ${PORT}`)
 })
